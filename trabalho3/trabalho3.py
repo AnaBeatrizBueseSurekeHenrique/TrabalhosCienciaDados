ff2d4865a05c92014363effafa9d15f153f4e938
AnaBeatrizBueseSurekeHenrique <138933836+AnaBeatrizBueseSurekeHenrique@users.noreply.github.com>
2026-04-22 14:11:57 -0300

    Trabalho 3

---
+++ trabalho3/trabalho3.py
@@ -0,0 +1,126 @@
+from time import sleep
+import pandas as pd
+import sqlalchemy as sqla
+import matplotlib.pyplot as plt
+import numpy
+import mysql.connector
+from mysql.connector import errorcode
+MIN = 2014
+MAX = 2017
+
+df = []
+for i in range(MIN, MAX):
+    df.append(pd.read_csv(
+        f'violencia_domestica_{i}.csv',
+        sep=';',
+        parse_dates=['data_fato'],
+        dtype={
+            'municipio_cod':'int64',
+            'municipio_fato': 'string'
+        },
+        na_values=['-', 'NA']
+    ))
+
+
+i = 0
+DB_NAME = 'trabalho3' 
+
+# Conecta ao servidor MySQL sem especificar um banco de dados inicial
+cnx = mysql.connector.connect(user='host',password='senha')
+cursor = cnx.cursor()
+
+def create_database(cursor):
+    #Cria o banco de dados especificado se ele não existir
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+    except mysql.connector.Error as err:
+        print("Falha ao criar banco de dados: {}".format(err))
+        exit(1)
+
+try:
+    # Tenta selecionar o banco de dados
+    cursor.execute("USE {}".format(DB_NAME))
+except mysql.connector.Error as err:
+    # Se o banco de dados não existir (erro ER_BAD_DB_ERROR)
+    print("Banco de dados {} não existe.".format(DB_NAME))
+    if err.errno ==  errorcode.ER_BAD_DB_ERROR:
+        create_database(cursor) # Chama a função para criar o banco de dados
+        print("Banco de dados {} criado com sucesso.".format(DB_NAME))
+        # Define o banco de dados ativo para a conexão
+        cnx.database = DB_NAME 
+    else:
+        # Outros erros são impressos e o programa é encerrado
+        print(err)
+        exit(1)
+
+
+TABLE_SCHEMA = """
+CREATE TABLE IF NOT EXISTS `violencia_contra_mulher` (
+  `municipio_cod` INT(6),
+  `municipio_fato` VARCHAR(32),
+  `data_fato` DATE,
+  `mes` INT(2),
+  `ano` INT(4),
+  `risp` VARCHAR(80),
+  `id` MEDIUMINT NOT NULL AUTO_INCREMENT,
+  `rmbh` VARCHAR(90),
+  `natureza_delito` VARCHAR(100),
+  `tentado_consumado` VARCHAR(9),
+  `qtde_vitimas` INT(2),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB
+"""
+
+try:
+    cursor.execute(TABLE_SCHEMA)
+    print("Tabela 'violencia' criada com sucesso.")
+    i = 0
+    print('AQUI')
+    for val in df:
+        if(i == 0):
+            val.to_sql("violencia_domestica",cnx, if_exists='replace')
+            i +=1
+        else:
+            val.to_sql("violencia_domestica",cnx, if_exists='append')
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+        print("Tabela 'violencia' já existe.")
+        cnx.shutdown()
+    else:
+        print(err.msg)
+finally:
+    if cnx.is_connected():
+        cursor.close()
+        cnx.disconnect()
+        cnx.close()
+        
+# Conexão
+
+# Banco de Dados
+
+# set_database()
+
+# get_database()
+
+# in_transaction()
+
+# config()
+
+# Consultas
+
+# cursor()
+
+# cmd_query()
+
+# cmd_query_iter()
+
+# get_rows() / get_row()
+
+# Outros
+
+# cmd_quit()
+
+# cmd_init_db()
+
+# cmd_query()
